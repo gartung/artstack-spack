ef333bf05eecb6d8f29ecaecdcc0235a1f9aed5d
Patrick Gartung <gartung@fnal.gov>
2016-06-08 13:09:43 -0500

    update tbb version

--- packages/art/package.py
+++ packages/art/package.py
@@ -3,7 +3,7 @@ from spack import *
 class Art(Package):
     homepage = "https://github.com/gartung/fnal-art.git"
 
-    version("dev", git="https://github.com/gartung/fnal-art.git",branch="aternate-cmake")
+    version("dev", git="https://github.com/gartung/fnal-art.git",branch="alternate-cmake")
 
     depends_on("canvas")
     depends_on("cetlib")
