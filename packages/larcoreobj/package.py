55736069b391d7e1ee2c56fecae75e7e3ff51698
Patrick Gartung <gartung@fnal.gov>
2016-06-21 16:49:12 -0500

    add the larsoftobj packages

---
+++ packages/larcoreobj/package.py
@@ -0,0 +1,30 @@
+from spack import *
+
+class Larcoreobj(Package):
+    homepage = "https://github.com/gartung/fnal-larcoreobj.git"
+
+    version("dev", git="https://github.com/gartung/fnal-larcoreobj.git",branch="alt-cmake")
+
+    depends_on("canvas")
+    depends_on("cetlib")
+    depends_on("fhicl-cpp")
+    depends_on("messagefacility")
+    depends_on("root@6.06.02")
+    depends_on("clhep+cxx14~cxx11")
+    depends_on("cmake@3.5:")
+    depends_on("cetbuildtools2")
+    depends_on("boost@1.60.0")
+
+    def install(self, spec, prefix):
+        with working_dir('build', create=True):
+            cmake_args = std_cmake_args
+            cmake_args += ["../"]
+            cmake_args += ["-DCET_COMPILER_WARNINGS_ARE_ERRORS=OFF"]
+            cmake_args += ["-DALT_CMAKE=1"]
+            cmake_args += ["-DCMAKE_CXX_FLAGS=-std=c++14"]
+            cmake_args += ["-DROOT_VERSION=6"]
+            cmake_args += ["-DHAVE_ROOT6=1"]
+            cmake(*cmake_args)
+            make("VERBOSE=1")
+            make("install")
+
