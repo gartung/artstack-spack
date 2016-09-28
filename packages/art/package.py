70afb6e18a0504800367de10259c0b8e737fb1c3
Patrick Gartung <gartung@fnal.gov>
2016-09-28 11:58:40 -0500

    revert cmake change

--- packages/art/package.py
+++ packages/art/package.py
@@ -12,7 +12,7 @@ class Art(Package):
     depends_on("root@6.06.02")
     depends_on("clhep")
     depends_on("tbb@20151115oss")
-    depends_on("cmake@3.5", type='build')
+    depends_on("cmake@3.5:", type='build')
     depends_on("cetbuildtools2", type='build')
     depends_on("boost@1.60.0")
 
