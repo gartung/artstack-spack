c4c05bb033690e93e643d6fa8388873e8420f22f
Patrick Gartung <gartung@fnal.gov>
2016-11-16 11:04:49 -0600

    add cmake dependency to cetbuildtools2

--- packages/cetbuildtools2/package.py
+++ packages/cetbuildtools2/package.py
@@ -7,6 +7,8 @@ class Cetbuildtools2(Package):
     version("0.1", "52b4f84b9fb3dc2ac8dee960c6adbca8")
     version("0.2", "40b9f8ab9cc51ac08e81fecacbca9954")
 
+    depends_on('cmake@3.5:', type='build')
+
     def install(self, spec, prefix):
         with working_dir('build', create=True):
             cmake_args = std_cmake_args
