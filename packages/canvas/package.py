f574c9697088627e3cffdb94ea81e1e406f80e50
Patrick Gartung <gartung@fnal.gov>
2016-11-16 12:50:33 -0600

    add cppunit to canvas deps

--- packages/canvas/package.py
+++ packages/canvas/package.py
@@ -10,6 +10,7 @@ class Canvas(Package):
     depends_on("messagefacility")
     depends_on("root@6.06.04")
     depends_on("clhep")
+    depends_on("cppunit")
     depends_on("tbb@20151115oss")
     depends_on("cetbuildtools2",type='build')
     depends_on("cmake",type='build')
