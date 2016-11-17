17ca6a6c090ad812121363ad6e01e5ac5ce8e507
Patrick Gartung <gartung@fnal.gov>
2016-11-17 11:37:43 -0600

    make cppunit for linux only

--- packages/canvas/package.py
+++ packages/canvas/package.py
@@ -1,4 +1,5 @@
 from spack import *
+import sys
 
 class Canvas(Package):
     homepage = "https://github.com/gartung/fnal-canvas.git"
@@ -10,7 +11,7 @@ class Canvas(Package):
     depends_on("messagefacility")
     depends_on("root@6.06.04")
     depends_on("clhep")
-    depends_on("cppunit")
+    if sys.platform == "linux2" : depends_on("cppunit")
     depends_on("tbb@20151115oss")
     depends_on("cetbuildtools2",type='build')
     depends_on("cmake",type='build')
